58e90cad86a03871c0506f6cd67e470c97fb23c5
Sergey Kabanov <me.fargot@ya.ru>
2019-01-20 14:16:01 +0300

    Переделать логику input_data
    Добавить временные переменные, на основе которых создавать namedtuple, убрать вызовы _replace()

--- lesson_5/task_1.py
+++ lesson_5/task_1.py
@@ -16,18 +16,13 @@ def input_data():
     Company = namedtuple('Company', 'name, profit', defaults=['Имя предприятия', [0, 0, 0, 0]])
 
     for i in range(1, number_company + 1):
-        current_company = Company()
-
-        current_company = current_company._replace(name=input(f'Введите название {i} компании: '))
-
-        cur_comp_profit = [0, 0, 0, 0]
+        tmp_name = input(f'Введите название {i} компании: ')
+        tmp_profit = [0, 0, 0, 0]
         for q in range(4):
-            cur_comp_profit[q] = int(input(f'Введите прибыль компании {current_company.name} '
-                                           f'за {q+1} квартал: '))
-        current_company = current_company._replace(profit=cur_comp_profit)
-
+            tmp_profit[q] = int(input(f'Введите прибыль компании {tmp_name} '
+                                      f'за {q+1} квартал: '))
+        current_company = Company(tmp_name, tmp_profit)
         companies.append(current_company)
-
     return companies
 
 
