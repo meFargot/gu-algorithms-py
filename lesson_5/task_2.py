5026d859c5310b896beea3497fcb3bac9ac2ad02
Sergey Kabanov <me.fargot@ya.ru>
2019-01-23 19:42:35 +0300

    Реализовать функцию add_numbers(a, b)

--- lesson_5/task_2.py
+++ lesson_5/task_2.py
@@ -5,16 +5,27 @@
 from collections import deque
 
 
-def input_hex_number():
-    """ Получает шестнадцатеричное число с клавиатуры, возвращает массив по условию ДЗ
-    """
-    pass
+HEX = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
 
 
 def add_numbers(a, b):
     """ Складывает два шестнадцатеричных числа в виде массивов. Возвращает массив по условию ДЗ
     """
-    pass
+    mem = 0  # "в уме" при сложении столбиком
+    sum_ = deque()
+    if len(a) < len(b):
+        a, b = b, a
+    for _ in range(len(a) - len(b)):  # дополняем нулями короткое число
+        b.appendleft('0')
+    for _ in range(len(a)):
+        cur_sum_num = HEX.index(a.pop()) + HEX.index(b.pop()) + mem
+        to_sum = cur_sum_num % 16
+        mem = cur_sum_num // 16
+        sum_.appendleft(HEX[to_sum])
+    if mem > 0:
+        sum_.appendleft(HEX[mem])
+    return sum_
+
 
 
 def mult_numbers(a, b):
@@ -23,15 +34,13 @@ def mult_numbers(a, b):
     pass
 
 
-print('Введите первое шестнадцатеричное число: ', end='')
-hex_num_a = input_hex_number()
-print('Введите второе шестнадцатеричное число: ', end='')
-hex_num_b = input_hex_number()
+hex_num_a = deque(input('Введите первое шестнадцатеричное число: '))
+hex_num_b = deque(input('Введите второе шестнадцатеричное число: '))
 
 print(f'Первое число в виде массива: {hex_num_a}\nВторое число в виде массива: {hex_num_b}')
 
 sum_a_b = add_numbers(hex_num_a, hex_num_b)
-print(f'Сумма чисел A и B = {sum_a_b}')
+print(f'Сумма чисел A и B = {list(sum_a_b)}')
 
 mult_a_b = mult_numbers(hex_num_a, hex_num_b)
 print(f'Произведение чисел A и B = {sum_a_b}')
