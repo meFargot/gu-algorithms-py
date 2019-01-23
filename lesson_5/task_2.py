7f4eb5e148a7efad6515318e11ba2d5f09ee702d
Sergey Kabanov <me.fargot@ya.ru>
2019-01-23 20:32:28 +0300

    task_2 готов

--- lesson_5/task_2.py
+++ lesson_5/task_2.py
@@ -11,36 +11,69 @@ HEX = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'
 def add_numbers(a, b):
     """ Складывает два шестнадцатеричных числа в виде массивов. Возвращает массив по условию ДЗ
     """
-    mem = 0  # "в уме" при сложении столбиком
     sum_ = deque()
+    a = deque(a)
+    b = deque(b)
+
     if len(a) < len(b):
         a, b = b, a
+
     for _ in range(len(a) - len(b)):  # дополняем нулями короткое число
         b.appendleft('0')
+
+    mem = 0  # "в уме" при сложении столбиком
     for _ in range(len(a)):
         cur_sum_num = HEX.index(a.pop()) + HEX.index(b.pop()) + mem
         to_sum = cur_sum_num % 16
         mem = cur_sum_num // 16
         sum_.appendleft(HEX[to_sum])
+
     if mem > 0:
         sum_.appendleft(HEX[mem])
-    return sum_
 
+    return sum_
 
 
 def mult_numbers(a, b):
     """ Умножает два шестнадцатеричных числа в виде массивов. Возвращает массив по условию ДЗ
     """
-    pass
+    mult = deque()
+    a = deque(a)
+    b = deque(b)
+
+    if len(a) < len(b):
+        a, b = b, a
+
+    for digit in range(len(b)):
+        cur_multiplier = HEX.index(b.pop())  # множитель текущего разряда
+        cur_digit_num = deque()  # произведение для текущего разряда
+
+        mem = 0  # "в уме" при умножении столбиком
+        a_tmp = deque(a)
+        for _ in range(len(a)):
+            cur_digit = (HEX.index(a_tmp.pop()) * cur_multiplier) + mem
+            to_digit_num = cur_digit % 16
+            mem = cur_digit // 16
+            cur_digit_num.appendleft(HEX[to_digit_num])
+
+        if mem > 0:
+            cur_digit_num.appendleft(HEX[mem])
+
+        for _ in range(digit):
+            cur_digit_num.append('0')  # дополняем нулями справа в зависимости от разряда
+
+        mult = add_numbers(mult, cur_digit_num)
+
+    return mult
 
 
 hex_num_a = deque(input('Введите первое шестнадцатеричное число: '))
 hex_num_b = deque(input('Введите второе шестнадцатеричное число: '))
 
-print(f'Первое число в виде массива: {hex_num_a}\nВторое число в виде массива: {hex_num_b}')
+print(f'Первое число в виде массива: {list(hex_num_a)}\nВторое число в виде массива: {list(hex_num_b)}')
 
 sum_a_b = add_numbers(hex_num_a, hex_num_b)
-print(f'Сумма чисел A и B = {list(sum_a_b)}')
+print(f'Сумма чисел = {list(sum_a_b)}')
 
 mult_a_b = mult_numbers(hex_num_a, hex_num_b)
-print(f'Произведение чисел A и B = {sum_a_b}')
+print(f'Произведение чисел = {list(mult_a_b)}')
